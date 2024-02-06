6270ebf5c4af2b2148d1a4fc33e63336e9cc0b37
simonieee <75366051+simonieee@users.noreply.github.com>
2024-02-06 17:56:04 +0900

    requirements 추가

---
+++ controllers/minilm_controller.py
@@ -0,0 +1,120 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException, APIRouter
+from models.minilm_model import vectorize_data, generate_metadata, format_vectors, search_vector, questrion_vectorize
+from pinecone import Pinecone, PodSpec
+from dotenv import load_dotenv
+import os
+import json
+
+minilm_router = APIRouter(tags=["paraphrase-multilingual-MiniLM-L12-v2 모델"])
+
+load_dotenv()
+# app = FastAPI()
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+
+@minilm_router.post("/create_db/", description="Vector DB 생성 metric: Cosine 고정")
+async def create_db(dbname: str, dimension: int):
+    try:
+        if pc.list_indexes():
+            status = pc.list_indexes()[0]
+            return {"message": "이미 생성된 Vector DB가 있습니다. 생성할 수 없습니다.", "db_name": status["name"], "dimension": status["dimension"]}
+        pc.create_index(dbname, dimension, metric="cosine",spec=PodSpec(environment="gcp-starter"))
+        return {"message": "Vector DB created successfully!"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@minilm_router.get("/db_status/", description="생성된 Vector DB 조회")
+async def db_status():
+    try:
+        if pc.list_indexes():
+            dblist = pc.list_indexes()[0]
+            return {"db_name": dblist["name"], "dimension": dblist["dimension"]}
+        else:
+            return {"message": "생성된 Vector DB가 없습니다."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))    
+    
+@minilm_router.post("/delete_db/", description="Vector DB 삭제")
+async def delete_db(dbname: str):
+    try:
+        if pc.list_indexes():
+            pc.delete_index(dbname)
+            return {"message": "Vector DB deleted successfully!"}
+        else:
+            return {"message": "삭제할 Vector DB가 없습니다."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@minilm_router.get("/db_dimensions/", description="현재 설정된 Vector DB dimensions 조회")
+async def get_db_dimensions():
+    try:
+        if pc.list_indexes():
+            dimension = pc.list_indexes()[0]["dimension"]
+            return {"dimension": dimension}
+        else :
+            return {"message":"생성된 Vector DB가 없습니다."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@minilm_router.post("/minilm_upload/", description="paraphrase-multilingual-MiniLM-L12-v2 모델 사용 Vector DB에 384차원 벡터 데이터 업로드")
+async def upload_file(dbname:str,  file: UploadFile = File(...)):
+    try:
+        index = pc.Index(dbname)
+        contents = await file.read()
+        data = json.loads(contents)
+        vectors = vectorize_data(data)
+        metadata_list = generate_metadata(data)
+        formatted_vectors = format_vectors(vectors, metadata_list)
+        index.upsert(vectors=formatted_vectors, namespace="job4")
+        return {"message": "Data uploaded successfully!"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@minilm_router.get("/minilm_jobsearch/", description="paraphrase-multilingual-MiniLM-L12-v2 모델 사용 질문에 대한 유사한 답변을 Vector DB에서 검색")
+async def job_search(dbname:str, question: str):
+    try:
+        index = pc.Index(dbname)
+        query_vector = questrion_vectorize(question)
+        search_results = search_vector(index, query_vector)
+        if not search_results.matches:
+            return {"message": "No matches found"}
+        result = {
+            "metadata": search_results.matches[0]["metadata"],
+            "score": search_results.matches[0]["score"]
+        }
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@minilm_router.post("/similarity_verification/", description="paraphrase-multilingual-MiniLM-L12-v2 모델 사용, 여러 질문에 대한 유사한 답변을 Vector DB에서 검색후 평균 스코어 및 결과 반환")
+async def similarity_verification(dbname:str, file: UploadFile = File(...)):
+    try:
+        index = pc.Index(dbname)
+        content = await file.read()
+        data = json.loads(content)
+        search_results = []
+        for item in data:
+            # 검색어 벡터화
+            query_vector = questrion_vectorize(item)
+
+            # Pinecone에서 벡터 검색
+            search_result = index.query(
+                namespace="job4",
+                vector=query_vector,
+                top_k=5,
+                include_values=True,
+                include_metadata=True
+            )
+            if search_result.matches:
+                result = {
+                    "keyword": item,
+                    "metadata": search_result.matches[0]["metadata"],
+                    "score": search_result.matches[0]["score"]
+                }
+                search_results.append(result)
+            else:
+                search_results.append({"keyword":item, "metadata":{}, "score": 0.0})
+            req = {"data":search_results, "avg_score": sum([item["score"] for item in search_results]) / len(search_results)}
+        return req
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from controllers.minilm_controller import minilm_router
+from controllers.large_controller import large_router 
+from controllers.db_controller import db_router
+app = FastAPI()
+
+# FastAPI 앱에 컨트롤러 라우트 추가 (모델별로 분류)
+app.include_router(minilm_router)
+app.include_router(large_router)
+app.include_router(db_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    
